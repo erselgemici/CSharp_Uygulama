b507b51190f87bae33aa2c77b1de2e3c3d64d853
Ersel <52753296+erselgemici@users.noreply.github.com>
2022-02-18 19:18:22 +0300

    1. Hafta Uygulamalar

---
+++ 1. Hafta C# Uygulama/Girilen sayi 0-100 arasi mi/Girilen sayi 0-100 arasi mi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Girilen_sayi_0_100_arasi_mi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            Console.Write("Sayıyı Giriniz : ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi > 0 && sayi < 100)
+            {
+                Console.WriteLine("Girilen sayı 0-100 aralığında");
+            }
+            else
+            {
+                Console.WriteLine("Girilen sayı 0-100 aralığında değil");
+            }
+        }
+    }
+}
