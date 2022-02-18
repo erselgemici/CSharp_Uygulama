ef543728889e8e3b1a33126506c5b1d550a6ee1a
Ersel <52753296+erselgemici@users.noreply.github.com>
2022-02-18 19:16:08 +0300

    1. Hafta Uygulamalar

---
+++ 1. Hafta C# Uygulama/Dikdörtegenin Alanı ve Cevresi/Dikdörtegenin Alanı ve Cevresi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Dikdörtegenin_Alanı_ve_Cevresi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kisakenar, uzunkenar, alan, cevre;
+
+            Console.Write("Kısa Kenar : ");
+            kisakenar = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Uzun Kenar : ");
+            uzunkenar = Convert.ToInt32(Console.ReadLine());
+
+            alan = kisakenar * uzunkenar;
+            Console.WriteLine("Alan : " + alan);
+
+            cevre = 2*(kisakenar + uzunkenar);
+            Console.Write("Çevre : " + cevre);
+        }
+    }
+}
