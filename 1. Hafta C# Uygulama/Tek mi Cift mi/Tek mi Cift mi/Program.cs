d589b74c079eaf7fe9fded93377273e153d9918e
Ersel <52753296+erselgemici@users.noreply.github.com>
2022-02-18 19:17:44 +0300

    1. Hafta Uygulamalar

---
+++ 1. Hafta C# Uygulama/Tek mi Cift mi/Tek mi Cift mi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tek_mi_Cift_mi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayı giriniz : ");
+
+            int sayi;
+            sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi % 2 == 0)
+            {
+                Console.WriteLine(sayi + " sayısı çift sayıdır.");
+            }
+            else
+            {
+                Console.WriteLine(sayi + " sayısı tek sayıdır.");
+            }
+            
+        }
+    }
+}
