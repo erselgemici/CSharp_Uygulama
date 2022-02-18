1442cd8546160de26e7a297ab61d98d9b1d24dc0
Ersel <52753296+erselgemici@users.noreply.github.com>
2022-02-18 19:19:57 +0300

    1. Hafta Uygulamalar

---
+++ 1. Hafta C# Uygulama/Cumledeki Harf Sayisi/Cumledeki Harf Sayisi/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Cumledeki_Harf_Sayisi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir Cümle Giriniz : ");
+            string cumle = Console.ReadLine();
+
+            string[] kelimeler = cumle.Split(' ');
+
+            Console.Write("Cümledeki Harf Sayısı : ");
+            Console.Write(cumle.Length - (kelimeler.Length - 1));
+        }
+    }
+}
