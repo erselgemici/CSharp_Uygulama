cb7cc7c29e3e1172e91018c7132c4e166cecf5c7
Ersel <52753296+erselgemici@users.noreply.github.com>
2022-02-18 19:14:22 +0300

    1. Hafta Uygulamalar

---
+++ 1. Hafta C# Uygulama/Iki Sayinin Toplami/Iki Sayinin Toplami/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Iki_Sayinin_Toplami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1, sayi2,toplam;
+
+            Console.Write("1. Sayı : ");
+            sayi1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("2. Sayı : ");
+            sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            toplam = sayi1 + sayi2;
+
+            Console.WriteLine("{0} ve {1} sayısının toplamı : {2}",sayi1,sayi2,toplam);
+
+            Console.ReadKey();
+
+        }
+    }
+}
