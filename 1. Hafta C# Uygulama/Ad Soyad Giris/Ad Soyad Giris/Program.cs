5f3f7eb0e2a7f2044370307d8e64c2f4f9b9cf60
Ersel <52753296+erselgemici@users.noreply.github.com>
2022-02-18 19:13:07 +0300

    1. Hafta Uygulamalar

---
+++ 1. Hafta C# Uygulama/Ad Soyad Giris/Ad Soyad Giris/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ad_Soyad_Giris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ad = "Ersel";
+            Console.WriteLine("Adınız :" + " " + ad);
+
+            string soyad = "Gemici";
+            Console.WriteLine("Soyadınız :" + " " + soyad);
+
+            Console.Write("Merhaba " + ad + " " + soyad);
+        }
+    }
+}
