079f15bcc3e43739f6c0e2ef664632cb6eb90a12
Ersel <52753296+erselgemici@users.noreply.github.com>
2022-02-24 18:07:44 +0300

    2. Hafta Uygulamalar

---
+++ 2. Hafta C# Uygulama/1-N arasi 3e ve 7ye Tam Bolunen Sayilar/1-N arasi 3e ve 7ye Tam Bolunen Sayilar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _1_N_arasi_3e_ve_7ye_Tam_Bolunen_Sayilar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Sayı Giriniz : "); 
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1 ; i < n; i++)
+
+            {
+                if (i % 3 == 0 && i % 7 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
