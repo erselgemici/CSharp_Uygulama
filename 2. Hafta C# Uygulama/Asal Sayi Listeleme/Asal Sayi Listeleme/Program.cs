1572b3fa79d3b70150a431cf1c7dd8bb22333706
Ersel <52753296+erselgemici@users.noreply.github.com>
2022-02-24 18:06:50 +0300

    2. Hafta Uygulamalar

---
+++ 2. Hafta C# Uygulama/Asal Sayi Listeleme/Asal Sayi Listeleme/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Asal_Sayi_Listeleme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kontrol = 0;
+            
+            for (int i = 2; i <= 1000000; i++)
+            {
+                kontrol = 0;
+
+                for (int j = 2; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        kontrol++;
+                        break;
+                    }
+                }
+                if (kontrol == 0)
+                {
+                    
+                    Console.Write(i + "-");
+                }
+
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
