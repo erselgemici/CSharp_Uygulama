f3d3f9e27939a8abe513bdfec899ce856be06129
Ersel <52753296+erselgemici@users.noreply.github.com>
2022-02-24 22:01:58 +0300

    2. Hafta Uygulamalar

---
+++ 2. Hafta C# Uygulama/Diziyi Alfabetik Siralama/Diziyi Alfabetik Siralama/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Diziyi_Alfabetik_Siralama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen veri adedini giriniz : ");
+            int adet;
+            adet = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Lütfen en sevdiğiniz {0} adet filmi giriniz. ", adet);
+
+            string[] film = new string[adet];
+
+            string TvSeries;
+            for (int i = 0; i < adet; i++)
+            {
+                Console.Write(i + 1 + ". filmi giriniz : ");
+                TvSeries = Console.ReadLine();
+                film[i] = TvSeries;
+            }
+            Console.WriteLine("=========================================================");
+            Console.WriteLine("Girdiğiniz filmlerin alfabetik sıralanışı şu şekildedir:");
+            Array.Sort(film);
+            foreach (string eleman in film)
+            {
+                Console.WriteLine(eleman);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
