c7a5e3a59dc00217ef54074d5d89d7c6a9368d1a
Ersel <52753296+erselgemici@users.noreply.github.com>
2022-02-25 16:55:20 +0300

    2. Hafta Uygulamalar

---
+++ 2. Hafta C# Uygulama/Sayilarin Toplami/Sayilarin Toplami/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sayilarin_Toplami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            int toplam = 0;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write("Lütfen {0}. sayıyı giriniz : ", i);
+                sayi = Convert.ToInt32(Console.ReadLine());
+                toplam += sayi;
+            }
+            Console.WriteLine("Girdiğiniz sayıların toplamı : {0}", toplam);
+            Console.ReadKey();
+        }
+    }
+}
