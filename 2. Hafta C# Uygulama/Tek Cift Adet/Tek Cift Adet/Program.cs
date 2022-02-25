23b33051663a9a910167ea5512ab1acfbcc4603b
Ersel <52753296+erselgemici@users.noreply.github.com>
2022-02-25 17:24:35 +0300

    2. Hafta Uygulamalar

---
+++ 2. Hafta C# Uygulama/Tek Cift Adet/Tek Cift Adet/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Tek_Cift_Adet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi, tekSayisi = 0, ciftSayisi = 0;
+            Random rnd = new Random();
+            for (int i = 1; i <= 10; i++)
+            {
+                sayi = rnd.Next();
+                Console.WriteLine(sayi);
+                if (sayi%2==0)
+                {
+                    ciftSayisi++;
+                }
+                else
+                {
+                    tekSayisi++;
+                }
+            }
+            Console.WriteLine("Verilerin {0} adedi çifttir.", ciftSayisi);
+            Console.WriteLine("Verilerin {0} adedi tektir.", tekSayisi);
+            Console.ReadKey();
+        }
+    }
+}
