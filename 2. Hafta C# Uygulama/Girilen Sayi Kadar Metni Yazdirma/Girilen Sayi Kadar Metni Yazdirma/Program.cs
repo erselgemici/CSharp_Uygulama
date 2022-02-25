5c46ca6b4add0281247a21c8982862018297a19a
Ersel <52753296+erselgemici@users.noreply.github.com>
2022-02-25 17:47:43 +0300

    2. Hafta Uygulamalar

---
+++ 2. Hafta C# Uygulama/Girilen Sayi Kadar Metni Yazdirma/Girilen Sayi Kadar Metni Yazdirma/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Girilen_Sayi_Kadar_Metni_Yazdirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen bir metin giriniz : ");
+            string metin = Console.ReadLine();
+            Console.WriteLine("Metin kaç defa yazdırılsın ?");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+
+            for (int i = 1; i < sayi; i++)
+            {
+                Console.WriteLine(metin);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
