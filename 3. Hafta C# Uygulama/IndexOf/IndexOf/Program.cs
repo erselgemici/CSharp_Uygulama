d4638c952daec281d8e080a9550a2f7a68615b92
Ersel <52753296+erselgemici@users.noreply.github.com>
2022-03-09 20:16:27 +0300

    3. Hafta Uygulamalar

---
+++ 3. Hafta C# Uygulama/IndexOf/IndexOf/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IndexOf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir metin giriniz : ");
+            string metin = Console.ReadLine();
+            int piece = metin.IndexOf(' ');
+            Console.WriteLine(metin.Substring(0,piece));
+            Console.ReadKey();
+        }
+    }
+}
