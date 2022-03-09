9dfe6c5eec45b64a9d7a64a946e7e246dc2f33a1
Ersel <52753296+erselgemici@users.noreply.github.com>
2022-03-09 20:15:49 +0300

    3. Hafta Uygulamalar

---
+++ 3. Hafta C# Uygulama/Celcius-Fahrenheit/Celcius-Fahrenheit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Celcius_Fahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celcius, fahrenheit;
+            Console.Write("Derece cinsinden sıcaklık değerini giriniz : ");
+            celcius = Convert.ToDouble(Console.ReadLine());
+            fahrenheit = (celcius * 9) / 5 + 32;
+            Console.WriteLine("{0}°C = {1}°F",celcius,fahrenheit);
+            Console.ReadLine();
+        }
+    }
+}
