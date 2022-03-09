61aafb8afb971ecd82a60f8f47e06c4c9bbbabdc
Ersel <52753296+erselgemici@users.noreply.github.com>
2022-03-09 20:13:06 +0300

    3. Hafta Uygulamalar

---
+++ 3. Hafta C# Uygulama/Girilen Sayiya Gore Islem/Girilen Sayiya Gore Islem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Girilen_Sayiya_Gore_Islem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi,sonuc;
+            Console.Write("Bir sayı giriniz : ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi%2==0)
+            {
+                sonuc = sayi / 2;
+                Console.WriteLine("Girilen sayı çifttir.\n{0} / 2 = {1}",sayi,sonuc);
+            }
+            else
+            {
+                sonuc = sayi * 2;
+                Console.WriteLine("Girilen sayı tektir.\n{0} * 2 = {1}",sayi,sonuc);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
