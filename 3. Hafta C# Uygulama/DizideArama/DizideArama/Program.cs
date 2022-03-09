01bd0a8d6c9263bcc184d2ed6fa4f2919b1e1df6
Ersel <52753296+erselgemici@users.noreply.github.com>
2022-03-09 20:15:11 +0300

    3. Hafta Uygulamalar

---
+++ 3. Hafta C# Uygulama/DizideArama/DizideArama/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DizideArama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strDizi = { "Ejder", "Beratiye", "Melike", "Ersel" };
+            Console.Write("Bir kelime giriniz : ");
+            string kelime = Console.ReadLine();
+            if (ara(strDizi, kelime))
+                Console.WriteLine(kelime + " dizide bulundu.");
+            else
+                Console.WriteLine(kelime + " dizide bulunamadı.");
+            
+        }
+        public static bool ara(string[] dizi, string aranan)
+        {
+            for (int i = 0; i < dizi.Length; i++)
+                if (aranan.Equals(dizi[i])) return true;
+            return false;
+        }
+    }
+    
+}
