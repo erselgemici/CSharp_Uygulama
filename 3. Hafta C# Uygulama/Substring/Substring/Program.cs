59375d9511fa29e406c237a499cb87e1ecf5eaee
Ersel <52753296+erselgemici@users.noreply.github.com>
2022-03-09 20:10:53 +0300

    3. Hafta Uygulamalar

---
+++ 3. Hafta C# Uygulama/Substring/Substring/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac = 0;
+            Console.Write("Metin giriniz : ");
+            string metin = Console.ReadLine();
+            Console.Write("Aranan ifadeyi giriniz : ");
+            string aranan = Console.ReadLine();
+
+            int metinuzunluk = metin.Length;
+            int arananuzunluk = aranan.Length;
+
+            for (int i = 0; i <= (metinuzunluk - arananuzunluk ); i++)
+            {
+                if (metin.Substring(i, arananuzunluk) == aranan)
+                {
+                    sayac++;
+                }
+            }
+            Console.WriteLine(sayac);
+            Console.ReadLine();
+        }
+    }
+}
