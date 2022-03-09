e8219b37c14f75e6dd3becf2359c87255e0d71cc
Ersel <52753296+erselgemici@users.noreply.github.com>
2022-03-09 20:13:53 +0300

    3. Hafta Uygulamalar

---
+++ 3. Hafta C# Uygulama/Factorial/Factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int factorial = 1;
+            Console.Write("Bir sayı giriniz : ");
+            int sayi = int.Parse(Console.ReadLine());
+
+            for (int i = sayi; i > 0; i--)
+            {
+                factorial = factorial * i;
+            }
+            Console.WriteLine("Faktöriyel Sonucu : {0}",factorial);
+
+            Console.ReadLine();
+        }
+    }
+}
