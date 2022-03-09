87faaef13a431d9c86bbf9db3f63db509709224e
Ersel <52753296+erselgemici@users.noreply.github.com>
2022-03-09 20:09:40 +0300

    3. Hafta Uygulamalar

---
+++ 3. Hafta C# Uygulama/Cumledeki Kelime Sayisini Bulma/Cumledeki Kelime Sayisini Bulma/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Cumledeki_Kelime_Sayisini_Bulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string metin;
+            Console.Write("Metni giriniz : ");
+            metin = Console.ReadLine();
+
+            string[] kelimeler = metin.Split(' ');
+            Console.WriteLine("Kelime Sayısı : " + kelimeler.Length);
+
+            foreach (var kelime in kelimeler)
+            {
+                Console.WriteLine(kelime);
+            }
+            Console.ReadLine();
+        }
+    }
+}
