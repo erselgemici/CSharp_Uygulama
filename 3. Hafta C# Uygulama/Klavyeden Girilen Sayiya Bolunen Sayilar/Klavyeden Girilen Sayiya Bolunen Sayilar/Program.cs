61d197076a090f7aec2764049c22a5ce5d4d2e81
Ersel <52753296+erselgemici@users.noreply.github.com>
2022-03-09 20:14:37 +0300

    3. Hafta Uygulamalar

---
+++ 3. Hafta C# Uygulama/Klavyeden Girilen Sayiya Bolunen Sayilar/Klavyeden Girilen Sayiya Bolunen Sayilar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Klavyeden_Girilen_Sayiya_Bolunen_Sayilar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int adet = 0;
+            Console.Write("Bir sayı giriniz : ");
+            int sayi;
+            sayi = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0} sayısının katları:",sayi);
+
+            for (int i = 1; i <= 200; i++)
+            {
+                if (i % sayi == 0)
+                {
+                    Console.WriteLine(i);
+                    adet++;
+                }
+            }
+
+            Console.WriteLine("1'den 200'e {0} sayısına bölünen {1} adet sayı vardır.",sayi,adet);
+            Console.ReadLine();
+        }
+    }
+}
